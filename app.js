02bbfca0c1fc1c43d95a590c82db71a483a39061
MSeoJun <mun1582w@naver.com>
2019-08-26 17:00:30 +0900

    GIF 채팅방

--- app.js
+++ app.js
@@ -30,18 +30,11 @@ app.set('port', process.env.PORT || 8005);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gif', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
+app.use(sessionMiddleware);
 app.use(flash());
 
 app.use((req, res, next) => {
